26f9d11fc81deab22b080f5ccfe3be7e01a3208d
Mathias Karstädt <mathias.karstaedt@gmail.com>
2017-01-27 11:15:18 +0100

    added tests for credentials

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const wordpress = require('wordpress')
+const { parse } = require('url')
 
-module.exports = function (req, res) {
+module.exports = function (request, response) {
+  const { query } = parse(request.url, true)
+
+  if (!query.username || !query.password) {
+    let noCredentialsError = new Error('no login credentials provided')
+    noCredentialsError.statusCode = 401
+    throw noCredentialsError
+  }
   return { message: 'Hello!' }
 }
--- test/index.js
+++ test/index.js
@@ -1,12 +1,26 @@
-const micro = require('micro');
-const test = require('ava');
-const listen = require('test-listen').default;
-const request = require('request-promise');
-const wordpress_service = require('../index.js');
+const micro = require('micro')
+const test = require('ava')
+const listen = require('test-listen').default
+const request = require('request-promise')
+const wordpressService = require('../index.js')
+const service = micro(wordpressService)
 
-test('my endpoint', async t => {
-  const service = micro(wordpress_service);
-  const url = await listen(service);
-  const body = await request(url);
-  t.deepEqual(JSON.parse(body).message, 'Hello!');
-});
+test.beforeEach(async (t) => {
+  t.context.url = await listen(service)
+})
+
+test('throws Unauthorized error without credentials', async (t) => {
+  const error = await t.throws(request(t.context.url))
+  t.is(error.statusCode, 401)
+})
+
+test('returns success with credentials', async (t) => {
+  const body = await request({
+    url: t.context.url,
+    qs: {
+      username: 'test',
+      password: 'test'
+    }
+  })
+  t.deepEqual(JSON.parse(body).message, 'Hello!')
+})
